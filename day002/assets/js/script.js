48c0862c81cfbeb6defcd12732e2059185000179
moshfiqur-code-guru <mdsadik3342@gmail.com>
2026-07-08 15:54:42 +0600

    animation

--- day002/assets/js/script.js
+++ day002/assets/js/script.js
@@ -8,3 +8,19 @@ window.addEventListener("scroll", function () {
         header.classList.remove("scrollable");
     }
 });
+
+window.addEventListener("DOMContentLoaded", addAClass);
+
+function addAClass() {
+    document.body.classList.add("animation-start")
+}
+
+
+$(document).ready(function () {
+    $('.projects').slick({
+        slidesToShow: 3,
+        dots: true,
+        autoplay: true,
+
+    });
+});
