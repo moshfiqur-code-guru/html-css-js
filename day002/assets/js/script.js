61eb0bcf64adbb16e5ccea25a782d30958fb3362
Rubel Ahmmed <ahmmedrubel.ad@gmail.com>
2026-07-14 11:04:21 +0600

    Video background and achievement section done

--- day002/assets/js/script.js
+++ day002/assets/js/script.js
@@ -15,7 +15,16 @@ function addAClass() {
     document.body.classList.add("animation-start")
 }
 
+const openButton = document.querySelector(".play-button");
+const video = document.querySelector(".video-overlay");
+const videoFrame = document.getElementById("video-frame")
 
+openButton.addEventListener("click", videoOpener)
+video.addEventListener("click",  ()=>video.classList.remove("show"))
+function videoOpener(){
+    video.classList.add("show")
+    videoFrame.src = "https://www.youtube.com/embed/WvCFX2BINZ4?autoplay=1&mute=1"
+}
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         center: true,
@@ -25,3 +34,5 @@ $(document).ready(function () {
         dots: true
     });
 });
+
+
