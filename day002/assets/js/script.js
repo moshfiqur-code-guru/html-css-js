44384e5d2fd9b66f0b5a622ae9898a675fd6a028
Rubel Ahmmed <ahmmedrubel.ad@gmail.com>
2026-07-19 11:22:47 +0600

    Last section

--- day002/assets/js/script.js
+++ day002/assets/js/script.js
@@ -1,4 +1,3 @@
-
 const header = document.getElementById("header-section");
 
 window.addEventListener("scroll", function () {
@@ -20,19 +19,34 @@ const video = document.querySelector(".video-overlay");
 const videoFrame = document.getElementById("video-frame")
 
 openButton.addEventListener("click", videoOpener)
-video.addEventListener("click",  ()=>video.classList.remove("show"))
-function videoOpener(){
+video.addEventListener("click", () => video.classList.remove("show"))
+
+function videoOpener() {
     video.classList.add("show")
     videoFrame.src = "https://www.youtube.com/embed/WvCFX2BINZ4?autoplay=1&mute=1"
 }
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         center: true,
-        items:2,
-        loop:true,
-        margin:40,
+        items: 2,
+        loop: true,
+        margin: 40,
         dots: true
     });
 });
 
+const sendBtn = document.getElementById("send-btn");
+const input = document.getElementById("email");
+sendBtn.classList.add("disabled")
+input.classList.add("disabled")
+input.addEventListener("input", () => {
+    if (input.value.trim() === "") {
+        sendBtn.classList.add("disabled")
+        input.classList.add("disabled")
+    } else {
+        sendBtn.classList.remove("disabled")
+        input.classList.remove("disabled")
+    }
+})
 
