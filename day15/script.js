b4a4f2e8d42272c65f5ce854742e62cb97e2c308
Rubel Ahmmed <ahmmedrubel.ad@gmail.com>
2026-07-30 10:42:37 +0600

    Dashboard

---
+++ day15/script.js
@@ -0,0 +1,36 @@
+//const result = document.getElementById("result");
+
+/*
+function checkEvenOdd() {
+    const givenNumber = document.getElementById("givenNumber").value;
+    let initialPart = "Give Number: " + givenNumber + " ";
+    // if (givenNumber % 2 === 0) {
+    //     result.value = initialPart + "is  Even"
+    // } else {
+    //     result.value = initialPart + "is  Odd"
+    // }
+    result.value = givenNumber % 2 === 0 ? initialPart + "is  Even" : initialPart + "is  Odd"
+} */
+
+/*
+function isPrime() {
+    const givenNumber = document.getElementById("givenNumber").value;
+    let initialPart = "Give Number: " + givenNumber + " ";
+    if (givenNumber < 2) {
+        result.value = initialPart + "is Prime"
+    }
+    for (let i = 2; i < givenNumber; i++) {
+        if (givenNumber % i === 0) {
+            result.value = initialPart + "is Not Prime";
+            break;
+        } else {
+            result.value = initialPart + "is Prime";
+        }
+    }
+}
+*/
+// const evenOddCheckBtn = document.getElementById("checkBtn");
+// const primeCheckBtn = document.getElementById("checkPrimeBtn");
+//
+// evenOddCheckBtn.addEventListener("click", checkEvenOdd);
+// primeCheckBtn.addEventListener("click", isPrime);
