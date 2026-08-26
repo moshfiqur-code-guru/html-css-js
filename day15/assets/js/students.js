caeed74c15de3fc22a72e733093d8f9b04060398
Rubel Ahmmed <ahmmedrubel.ad@gmail.com>
2026-08-26 10:37:41 +0600

    bug fixing, image preview etc

--- day15/assets/js/students.js
+++ day15/assets/js/students.js
@@ -5,7 +5,9 @@ let modalTitle = "Add New Students";
 let modalButtonLabel = "Add New"
 const modalTitleElement = document.getElementById("modal-1-title");
 const modalButtonElement = document.getElementById("modalSaveButton");
-const studentForm = document.getElementById("studentForm")
+const studentForm = document.getElementById("studentForm");
+const studentImagePicker = document.getElementById("student-image");
+const preview = document.getElementById("thumbnail");
 let editableIndex = null;
 // const students = [{
 //     id: new Date().getMilliseconds(),
@@ -25,7 +27,9 @@ let editableIndex = null;
 //     },
 // ];
 //console.log(studentss)
-const students = JSON.parse(localStorage.getItem("students"))
+
+const students = JSON.parse(localStorage.getItem("students")) ?? []
+
 //->https://demo.restapi.org/get-students
 let tableBody = document.getElementById("tableBody");
 
@@ -64,12 +68,13 @@ let tableBody = document.getElementById("tableBody");
 
 function displayStudents() {
     tableBody.innerHTML = "";
-    students.forEach(function (student, index) {
-        const row = document.createElement("tr");
-        row.innerHTML = `
+    if (students.length > 0) {
+        students.forEach(function (student, index) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
         <td>${index + 1}</td>
         <td>${student.firstname} ${student.lastname}</td>
-        <td><img src="assets/img/author.jpeg"/></td>
+        <td><img src="assets/img/${student.image}"/></td>
         <td>${student.class}</td>
         <td>${student.id}</td>
         <td>${student.email}</td>
@@ -79,8 +84,13 @@ function displayStudents() {
             <button class="edit"  onclick="editStudent(${index}, ${student.id})" data-micromodal-trigger="modal-1"><i class="fa fa-solid fa-pencil"></i></button>
         </td>
         `;
+            tableBody.appendChild(row)
+        });
+    } else {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td colspan="8"><h2>Oops! Student data is not found.</h2></td>`;
         tableBody.appendChild(row)
-    });
+    }
 }
 
 displayStudents();
@@ -89,19 +99,38 @@ function saveStudents() {
     localStorage.setItem("students", JSON.stringify(students))
 }
 
+
+studentImagePicker.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const url = URL.createObjectURL(file);
+        preview.src = url;
+    }
+})
+
+//SAVE STUDENT OR UPDATE STUDENT
 modalSaveButton.addEventListener("click", (event) => {
     event.preventDefault();
     const formData = new FormData(studentForm);
     const student = Object.fromEntries(formData);
-    if (editableIndex !== null) {
+    student["image"] = student.image?.name
 
+    //Only for updating student and
+    // need that editable index present
+    if (editableIndex !== null) {
+        students[editableIndex] = student
     } else {
+        // this is necessary only for creating new
         student["id"] = new Date().getMilliseconds()
         students.push(student)
     }
+    //save updated students in the storage
     saveStudents();
+    //display again the updated students in the table
     displayStudents();
+    //after saving or updating student resetting the form
     studentForm.reset();
+
     MicroModal.close('modal-1')
 })
 
@@ -122,7 +151,26 @@ function editStudent(index, id) {
     //     return student.id === id
     // });
     // const student = students.find(student => student.id === id);
+    MicroModal.show('modal-1')
     const student = students[index];
+    // studentForm.elements["firstname"].value = student["firstname"]
+    // studentForm.elements["lastname"].value = student["lastname"]
+    // studentForm.elements["class"].value = student["class"];
+    const keys = Object.keys(student);
+    // for (let i = 0; i < keys.length; i++) {
+    //     if (keys[i] !== "image" && keys[i] !== "id") {
+    //         studentForm.elements[keys[i]].value = student[keys[i]]
+    //     }
+    // }
+    keys.forEach((key, index) => {
+        if (key !== "image" && key !== "id") {
+            studentForm.elements[key].value = student[key]
+        } else {
+            if (key === "image") {
+                preview.src = "assets/img/" + student.image
+            }
+        }
+    });
 }
 
 function setModalTitleAndButton() {
@@ -133,7 +181,12 @@ function setModalTitleAndButton() {
 function setDefaultTitle() {
     modalTitle = "Add New Student";
     modalButtonLabel = "Add New";
-    setModalTitleAndButton()
+    setModalTitleAndButton();
+    MicroModal.show('modal-1')
+}
+
+function onModalClose() {
+    preview.src = "assets/img/author.jpeg"
 }
 
 //saveStudents()
